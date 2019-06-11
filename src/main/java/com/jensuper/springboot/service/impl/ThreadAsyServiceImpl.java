c8b328125870416e5ade8aa80b83fe2a237002ad
JenSuper <253150284@qq.com>
2019-06-11 11:54:43 +0800

    异步线程池注解方式

---
+++ src/main/java/com/jensuper/springboot/service/impl/ThreadAsyServiceImpl.java
@@ -0,0 +1,35 @@
+package com.jensuper.springboot.service.impl;
+
+import com.jensuper.springboot.service.ThreadAsyService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+/**
+ * All rights Reserved, Designed By www.rongdasoft.com
+ *
+ * @version V1.0
+ * @Title: ThreadAsyServiceImpl
+ * @Description:
+ * @author:jichao
+ * @date: 2019/6/11
+ * @Copyright: 2019/6/11 www.rongdasoft.com
+ * Inc. All rights reserved.
+ */
+@Service
+@Slf4j
+public class ThreadAsyServiceImpl implements ThreadAsyService {
+
+    @Override
+    @Async
+    public void sycService() {
+        log.info("ThreadAsyServiceImpl ----START----");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.info("ThreadAsyServiceImpl ----END----");
+    }
+}
